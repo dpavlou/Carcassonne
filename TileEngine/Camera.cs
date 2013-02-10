7aad0f768a937a016f09dac73c5230584d32e944
Demos Pavlou <pavloud@gmail.com>
2013-02-10 17:53:42 +0100

    Renamed Tile Engine directory

---
+++ TileEngine/Camera.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TileEngine
+{
+   public static class Camera
+    {
+        #region Declarations
+        private static Vector2 position = Vector2.Zero;
+        private static Vector2 viewPortSize = Vector2.Zero;
+        private static Rectangle worldRectangle = new Rectangle(0, 0, 0, 0);
+        public static Matrix transform;
+        private static Vector2 centre;
+        public static Viewport viewport;
+        private static float zoom = 1;
+        private static float rotation = 0;
+
+        #endregion
+
+        #region Properties
+
+        static public Matrix Transform
+        {
+            get { return transform; }
+        }
+
+
+       
+       public static float Zoom
+       {
+           get { return zoom; }
+           set
+           {
+               zoom = value;
+               if (zoom < 0.1f)
+                   zoom = 0.1f;
+           }
+       }
+
+       public static float Rotation
+       {
+           get { return rotation; }
+           set { rotation = value; }
+       }
+
+       static public Viewport newViewPort
+       {
+          set { viewport = value; }
+       }
+
+       public static void Update()
+       {
+           centre = new Vector2(position.X,position.Y);
+           transform = Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
+                                               Matrix.CreateRotationZ(Rotation) *
+                                              Matrix.CreateScale(new Vector3(Zoom, Zoom, 0)) *
+                                              Matrix.CreateTranslation(new Vector3(viewport.Width / 3, viewport.Height / 3, 0));
+       }
+       
+
+       public static Vector2 Position
+        {
+            get { return position; }
+            set
+            {
+                position = new Vector2(
+                    MathHelper.Clamp(value.X,
+                        worldRectangle.X, worldRectangle.Width -
+                        ViewPortWidth),
+                    MathHelper.Clamp(value.Y,
+                        worldRectangle.Y, worldRectangle.Height -
+                        ViewPortHeight));
+            }
+        }
+
+        public static Rectangle WorldRectangle
+        {
+            get { return worldRectangle; }
+            set { worldRectangle = value; }
+        }
+
+        public static int ViewPortWidth
+        {
+            get { return (int)viewPortSize.X; }
+            set { viewPortSize.X = value; }
+        }
+
+        public static int ViewPortHeight
+        {
+            get { return (int)viewPortSize.Y; }
+            set { viewPortSize.Y = value; }
+        }
+
+        public static Rectangle ViewPort
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)Position.X, (int)Position.Y,
+                    ViewPortWidth, ViewPortHeight);
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public static void Move(Vector2 offset)
+        {
+            Position += offset;
+        }
+
+        public static bool ObjectIsVisible(Rectangle bounds)
+        {
+            return (ViewPort.Intersects(bounds));
+        }
+
+        public static Vector2 WorldToScreen(Vector2 worldLocation)
+        {
+            return worldLocation - position;
+        }
+
+        public static Rectangle WorldToScreen(Rectangle worldRectangle)
+        {
+            return new Rectangle(
+                worldRectangle.Left - (int)position.X,
+                worldRectangle.Top - (int)position.Y,
+                worldRectangle.Width,
+                worldRectangle.Height);
+        }
+
+        public static Vector2 ScreenToWorld(Vector2 screenLocation)
+        {
+            return screenLocation + position;
+        }
+
+        public static Rectangle ScreenToWorld(Rectangle screenRectangle)
+        {
+            return new Rectangle(
+                screenRectangle.Left + (int)position.X,
+                screenRectangle.Top + (int)position.Y,
+                screenRectangle.Width,
+                screenRectangle.Height);
+        }
+        #endregion
+
+    }
+}
---
+++ TileEngine/TileGrid.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TileEngine
+{
+    public static class TileGrid
+    {
+        #region Declarations
+        static public int TileWidth = 51;
+        static public int TileHeight = 51;
+        public const int OriginalTileWidth = 51;
+        public const int OriginalTileHeight = 51;
+        public const int MapWidth = 150;
+        public const int MapHeight = 150;
+        public const int MapLayers = 3;
+        private const int skyTile = 2;
+        static public Vector2 MapLocation = new Vector2(10, 10);
+
+        public static bool ShowMap = true;
+        static private Tile[,] mapCells =
+            new Tile[MapWidth, MapHeight];
+
+        public static bool EditorMode = false;
+
+        public static SpriteFont spriteFont;
+        static private Texture2D tileSheet;
+        #endregion
+
+        #region Initialization
+        static public void Initialize(Texture2D tileTexture)
+        {
+            Random rand = new Random();
+            tileSheet = tileTexture;
+
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    for (int z = 0; z < MapLayers; z++)
+                    {
+                        if (rand.Next(0,30) ==0)
+                            mapCells[x, y] = new Tile(skyTile, 0, 1, "");
+                        else
+                            mapCells[x, y] = new Tile(skyTile, 0, 0, "");
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Tile and Tile Sheet Handling
+        public static int TilesPerRow
+        {
+            get { return tileSheet.Width / OriginalTileWidth; }
+        }
+
+        public static Rectangle TileSourceRectangle(int tileIndex)
+        {
+            return new Rectangle(
+                (tileIndex % TilesPerRow) * OriginalTileWidth,
+                (tileIndex / TilesPerRow) * OriginalTileHeight,
+                OriginalTileWidth,
+                OriginalTileHeight);
+        }
+        #endregion
+
+        #region Information about Map Cells
+        static public int GetCellByPixelX(int pixelX)
+        {
+            return pixelX / TileWidth;
+        }
+
+        static public int GetCellByPixelY(int pixelY)
+        {
+            return pixelY / TileHeight;
+        }
+
+        static public Vector2 GetCellByPixel(Vector2 pixelLocation)
+        {
+            return new Vector2(
+                GetCellByPixelX((int)pixelLocation.X),
+                GetCellByPixelY((int)pixelLocation.Y));
+        }
+
+        static public Vector2 GetCellCenter(int cellX, int cellY)
+        {
+            return new Vector2(
+                (cellX * TileWidth) + (TileWidth / 2),
+                (cellY * TileHeight) + (TileHeight / 2));
+        }
+
+        static public Vector2 GetCellCenter(Vector2 cell)
+        {
+            return GetCellCenter(
+                (int)cell.X,
+                (int)cell.Y);
+        }
+
+        static public Rectangle CellWorldRectangle(int cellX, int cellY)
+        {
+            return new Rectangle(
+                cellX * TileWidth,
+                cellY * TileHeight,
+                TileWidth,
+                TileHeight);
+        }
+
+        static public Rectangle MapToScreenRectangle(int cellX, int cellY)
+        {
+            return new Rectangle(
+            cellX * TileWidth / 4 + (int)MapLocation.X,
+            cellY * TileHeight / 4 + (int)MapLocation.Y,
+            TileWidth / 4,
+            TileHeight / 4);
+        }
+
+        static public Rectangle CellWorldRectangle(Vector2 cell)
+        {
+            return CellWorldRectangle(
+                (int)cell.X,
+                (int)cell.Y);
+        }
+
+        static public Rectangle CellScreenRectangle(int cellX, int cellY)
+        {
+            return Camera.WorldToScreen(CellWorldRectangle(cellX, cellY));
+        }
+
+        static public Rectangle CellSreenRectangle(Vector2 cell)
+        {
+            return CellScreenRectangle((int)cell.X, (int)cell.Y);
+        }
+
+        static public bool CellIsPassable(int cellX, int cellY)
+        {
+            Tile square = GetMapSquareAtCell(cellX, cellY);
+
+            if (square == null)
+                return false;
+            else
+                return square.Passable;
+        }
+        
+        static public bool CellIsPassable(Vector2 cell)
+        {
+            return CellIsPassable((int)cell.X, (int)cell.Y);
+        }
+
+        static public bool CellIsPassableByPixel(Vector2 pixelLocation)
+        {
+            return CellIsPassable(
+                GetCellByPixelX((int)pixelLocation.X),
+                GetCellByPixelY((int)pixelLocation.Y));
+        }
+
+        static public string CellCodeValue(int cellX, int cellY)
+        {
+            Tile square = GetMapSquareAtCell(cellX, cellY);
+
+            if (square == null)
+                return "";
+            else
+                return square.CodeValue;
+        }
+
+        static public string CellCodeValue(Vector2 cell)
+        {
+            return CellCodeValue((int)cell.X, (int)cell.Y);
+        }
+
+        static public bool kouroupettos()
+        {
+            return true;
+        }
+        #endregion
+
+        #region Information about MapSquare objects
+        static public Tile GetMapSquareAtCell(int tileX, int tileY)
+        {
+            if ((tileX >= 0) && (tileX < MapWidth) &&
+                (tileY >= 0) && (tileY < MapHeight))
+            {
+                return mapCells[tileX, tileY];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static public void SetMapSquareAtCell(
+            int tileX,
+            int tileY,
+            Tile tile)
+        {
+            if ((tileX >= 0) && (tileX < MapWidth) &&
+                (tileY >= 0) && (tileY < MapHeight))
+            {
+                mapCells[tileX, tileY] = tile;
+            }
+        }
+
+        static public void SetTileAtCell(
+            int tileX,
+            int tileY,
+            int layer,
+            int tileIndex)
+        {
+            if ((tileX >= 0) && (tileX < MapWidth) &&
+                (tileY >= 0) && (tileY < MapHeight))
+            {
+                mapCells[tileX, tileY].LayerTiles[layer] = tileIndex;
+            }
+        }
+
+        static public Tile GetMapSquareAtPixel(int pixelX, int pixelY)
+        {
+            return GetMapSquareAtCell(
+                GetCellByPixelX(pixelX),
+                GetCellByPixelY(pixelY));
+        }
+
+        static public Tile GetMapSquareAtPixel(Vector2 pixelLocation)
+        {
+            return GetMapSquareAtPixel(
+                (int)pixelLocation.X,
+                (int)pixelLocation.Y);
+        }
+
+        #endregion
+
+        #region Loading and Saving Maps
+        public static void SaveMap(FileStream fileStream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, mapCells);
+            fileStream.Close();
+        }
+
+        public static void LoadMap(FileStream fileStream)
+        {
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                mapCells = (Tile[,])formatter.Deserialize(fileStream);
+                fileStream.Close();
+            }
+            catch
+            {
+                ClearMap();
+            }
+        }
+
+        public static void ClearMap()
+        {
+            for (int x = 0; x < MapWidth; x++)
+                for (int y = 0; y < MapHeight; y++)
+                    for (int z = 0; z < MapLayers; z++)
+                    {
+                        mapCells[x, y] = new Tile(skyTile, 0, 0, "");
+                    }
+        }
+        #endregion
+
+        #region Drawing
+        static public void Draw(SpriteBatch spriteBatch)
+        {
+            int startX = GetCellByPixelX((int)Camera.Position.X);
+            int endX = GetCellByPixelX((int)Camera.Position.X +
+                  Camera.ViewPortWidth);
+
+            int startY = GetCellByPixelY((int)Camera.Position.Y);
+            int endY = GetCellByPixelY((int)Camera.Position.Y +
+                      Camera.ViewPortHeight);
+
+            for (int x = startX; x <= endX; x++)
+                for (int y = startY; y <= endY; y++)
+                {
+                    for (int z = 0; z < MapLayers; z++)
+                    {
+                        if ((x >= 0) && (y >= 0) &&
+                            (x < MapWidth) && (y < MapHeight))
+                        {
+                            spriteBatch.Draw(
+                              tileSheet,
+                              CellScreenRectangle(x, y),
+                              TileSourceRectangle(mapCells[x, y].LayerTiles[z]),
+                              Color.White,
+                              0.0f,
+                              Vector2.Zero,
+                              SpriteEffects.None,
+                              1f - ((float)z * 0.1f));
+                        }
+                    }
+
+                    if (EditorMode)
+                    {
+                        DrawEditModeItems(spriteBatch, x, y);
+                    }
+
+                    if (ShowMap)
+                    {
+                        //  DrawMapOnScreen(spriteBatch, MapLocation);
+                    }
+
+                }
+        }
+
+        public static void DrawMapOnScreen(SpriteBatch spriteBatch, Vector2 MapLocation)
+        {
+
+            for (int x = 0; x <= MapWidth; x++)
+                for (int y = 0; y <= MapHeight; y++)
+                {
+                    for (int z = MapLayers - 1; z < MapLayers; z++)
+                    {
+                        if ((x >= 0) && (y >= 0) &&
+                            (x < MapWidth) && (y < MapHeight))
+                        {
+                            spriteBatch.Draw(
+                              tileSheet,
+                              MapToScreenRectangle(x, y),
+                              TileSourceRectangle(mapCells[x, y].LayerTiles[z]),
+                              Color.White,
+                              0.0f,
+                              Vector2.Zero,
+                              SpriteEffects.None,
+                              0.0f);
+                        }
+                    }
+                }
+        }
+        public static void DrawEditModeItems(
+            SpriteBatch spriteBatch,
+            int x,
+            int y)
+        {
+            if ((x < 0) || (x >= MapWidth) ||
+                (y < 0) || (y >= MapHeight))
+                return;
+
+
+            if (mapCells[x, y].CodeValue != "")
+            {
+                Rectangle screenRect = CellScreenRectangle(x, y);
+
+                spriteBatch.DrawString(
+                    spriteFont,
+                    mapCells[x, y].CodeValue,
+                    new Vector2(screenRect.X, screenRect.Y),
+                    Color.White,
+                    0.0f,
+                    Vector2.Zero,
+                    1.0f,
+                    SpriteEffects.None,
+                    0.0f);
+            }
+        }
+        #endregion
+
+    }
+}
