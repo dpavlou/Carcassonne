7dfb4c97ce9777faa43a9f20105ce03b86461489
gmich <g.mich90@gmail.com>
2013-02-10 18:29:51 +0200

    new ID

--- Carcassonne/Carcassonne/Game1.cs
+++ Carcassonne/Carcassonne/Game1.cs
@@ -68,7 +68,7 @@ namespace Carcassonne
             Camera.ViewPortWidth = 1600;
             Camera.ViewPortHeight = 900;
 
-            player = new Player(Content,"Kokos1");
+            player = new Player(Content,"skata");
 
             // TODO: use this.Content to load your game content here
         }
