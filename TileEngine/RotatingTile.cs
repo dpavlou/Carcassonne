d41c7489c350317e8662d866eb41362aaa9b44e5
gmich <g.mich90@gmail.com>
2013-02-11 06:06:31 +0200

    This .cs doesnt do anything yet

---
+++ TileEngine/RotatingTile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TileEngine
+{
+    public class RotatingTile
+    {
+       public bool clockwise;
+       public bool alive;
+        public static float rotationRate = (MathHelper.PiOver2 / 10);
+        private float rotationAmount = 0;
+        public int rotationTicksRemaining = 10;
+
+        public float RotationAmount
+        {
+            get
+            {
+                if (clockwise)
+                    return rotationAmount;
+                else
+                    return (MathHelper.Pi * 2) - rotationAmount;
+            }
+        }
+
+        public RotatingTile(bool clockwise,float rotationAmount)
+        {
+            this.clockwise = clockwise;
+            this.rotationAmount = rotationAmount;
+            rotationTicksRemaining = 10;
+            alive = true;
+        }
+
+        public bool Alive
+        {
+            get { return alive; }
+        }
+
+        public void UpdateRotation()
+        {
+            rotationAmount += rotationRate;
+            if (rotationTicksRemaining == 0)
+                alive = false;
+
+            rotationTicksRemaining = (int)MathHelper.Max(
+                0,
+                rotationTicksRemaining - 1);
+      
+        }
+    }
+}
