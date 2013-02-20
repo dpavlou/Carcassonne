971d7d46597b1fade25820fb17289a9dc083a2dd
Costas Michaelides <michaelidesc@gmail.com>
2013-02-20 05:41:11 +0200

    asd

--- Carcassonne/Carcassonne/Program.cs
+++ Carcassonne/Carcassonne/Program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace Carcassonne
 {
-#if WINDOWS || XBOX
     static class Program
     {
         /// <summary>
@@ -16,6 +15,5 @@ namespace Carcassonne
             }
         }
     }
-#endif
 }
 
